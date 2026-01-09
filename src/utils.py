d3fdfc93cab7862f59a11c041193a193f4d1e0a8
Ayush Khanal <ayukhanalsh100@gmail.com>
2026-01-10 01:28:23 +0545

    deployed wesite using flask

--- src/utils.py
+++ src/utils.py
@@ -14,3 +14,11 @@ def save_objec(file_path, object):
             dill.dump(object, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
